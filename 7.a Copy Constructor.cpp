aeceea5dfa6e0698f819ad0a296358d91528460f
Amisha <amisha.kr29@gmail.com>
2025-07-05 17:27:34 +0530

    added

--- 7.a Copy Constructor.cpp
+++ 7.a Copy Constructor.cpp
@@ -8,4 +8,4 @@ class{
 
 //A copy constructor is a special type of constructor in object-oriented programming that creates a new object as a copy of an existing object. It is typically used when an object is passed by value or returned by value, allowing a new instance of the object to be created as an exact copy of the original one.
 
-// In many programming languages, the copy constructor takes a reference to an object of the same type as a parameter and creates a new object with the same values as the original object.
+//The copy constructor takes a reference to an object of the same type as a parameter and creates a new object with the same values as the original object.
